aa83e5d9ccd5d5b777c78edbc7ca5db552d6d038
Rich Frost <richardfrost@economist.com>
2025-02-25 14:03:29 +0000

    Added base server scaffolding

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-// TODO
+import express from "express";
+import helmet from "helmet";
+
+export const runServer = () => {
+  const app = express();
+
+  app.use(helmet());
+  app.use(express.json());
+
+  const port = Number(process.env.APP_PORT) || 3080;
+
+  app.listen(port, () => {
+    console.log(`Server running as http://localhost:${port}`);
+  });
+};
+
+export const server = runServer();
