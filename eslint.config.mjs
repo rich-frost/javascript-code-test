52ddc404e8b285180b6daf6d36b61fc9e723cd8a
Rich Frost <richardfrost@economist.com>
2025-02-25 19:56:41 +0000

    Linter config to ignore coverage folder and original code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,6 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { ignores: ["coverage", "__original__"] },
+  { rules: { "@typescript-eslint/no-explicit-any": "off" } },
 ];
